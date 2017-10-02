a3c837ccd59da6def6f771ab6ae79b82ad53d1a1
robtplayground <rob.thwaites@playgroundxyz.com>
2017-10-03 08:32:41 +1100

    mrt content

--- js/site.js
+++ js/site.js
@@ -167,6 +167,7 @@ $(document).ready(function() {
   var sliderDownload = makeSlider("slider-download");
   var sliderWattyl1 = makeSlider("slider-wattyl1");
   var sliderWattyl2 = makeSlider("slider-wattyl2");
+  var sliderPoetry = makeSlider("slider-poetry");
 
   $('#wattyl-video').on('click', function(){
     $(this).removeClass('placeholder').children('video').get(0).play();
